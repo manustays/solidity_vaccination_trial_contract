7170c68087815b8fd1b93eca082e58b9e6835a70
Kumar Abhishek <manustays@users.noreply.github.com>
2021-06-19 15:35:20 +0530

    Update VaccineClinicalTrial.sol

--- VaccineClinicalTrial.sol
+++ VaccineClinicalTrial.sol
@@ -25,18 +25,11 @@ contract Ownable {
 }
 
 
-contract Clinics {
-    
-    struct Clinic {
-        uint64 id;
-        string location;
-        bool isApproved;
-    }
-    
-}
 
-
-contract Volunteers is Ownable {
+/**
+ * @title Base contract to keep track of authorized volunteers 
+ */
+contract Volunteers {
     
     struct Volunteer {
         uint256 dob;
@@ -71,15 +64,16 @@ contract Volunteers is Ownable {
         
         require(volunteers[volunteer].doesExist != true, "Volunteer already exists");
         
-        volunteers[volunteer].doesExist = true;
-        volunteers[volunteer].isApproved = false;
-        
-        volunteers[volunteer].dob = dob;
-        volunteers[volunteer].gender = gender;
-        volunteers[volunteer].medical_precondition_hash = sha256(medical_precondition);
-        volunteers[volunteer].other_vaccine_taken = other_vaccine_taken;
-        volunteers[volunteer].other_vaccine_name = other_vaccine_name;
-        volunteers[volunteer].other_vaccination_date = other_vaccination_date;
+        volunteers[volunteer] = Volunteer({
+            doesExist: true,
+            isApproved: false,
+            dob: dob,
+            gender: gender,
+            medical_precondition_hash: sha256(medical_precondition),
+            other_vaccine_taken: other_vaccine_taken,
+            other_vaccine_name: other_vaccine_name,
+            other_vaccination_date: other_vaccination_date
+        });
         
         emit VolunteerAdded(volunteer);
     }
@@ -89,17 +83,166 @@ contract Volunteers is Ownable {
         volunteers[msg.sender].doesExist = false;
     }
     
-    
-    /**
-     * 
-     */
-    function _approveVolunteer(address volunteer) internal {
-        volunteers[volunteer].isApproved = true;
-    }
-    
-    
     function isValidVolunteer(address volunteer) public view returns (bool) {
         return volunteers[volunteer].doesExist && volunteers[msg.sender].isApproved;
     }
+    
+    
+    function _approveVolunteer(address volunteer) internal {
+        volunteers[volunteer].isApproved = true;
+        
+        emit VolunteerApproved(volunteer);
+    }
+    
+    
+    function doesVolunteerExist(address volunteer) internal view returns (bool) {
+        return volunteers[volunteer].doesExist;
+    }
 
 }
+
+
+
+
+/**
+ * @title The Vaccine CLinical Trials contract
+ */
+contract ClinicalTrial is Ownable, Volunteers {
+    
+    address private _owner;
+    address private _clinicAuditor;
+    
+    bool _trialEnded;
+    
+    uint public NumberOfDose;
+    string private _feedbackPerDose;
+    
+    enum _clinicState { NotVerified, Verified, NotFunctional }
+    enum _volunteerState { NotVerified, Verified, InTrial, NotInTrial, CompletedTrial }
+    
+    struct Clinics {
+        address id;
+        string name;
+        string location;
+        string phoneNumber;
+        _clinicState clinicState;
+    }
+    
+    mapping(address => Clinics) clinics;
+    
+    event DoseGiven(address volunteer, uint doseNumber, uint remainingDaysForNextDose); // TODO: are we going to pass address of the volunteer ?
+    
+    
+    constructor (address clinicAuditor, uint numberOfDoses) {
+        _owner = msg.sender;
+        _clinicAuditor = clinicAuditor;
+        NumberOfDose = numberOfDoses;
+    }
+    
+    
+    function enrolClinic (string memory clinicName, string memory clinicLocation, string memory clinicPhoneNumber) public {
+        Clinics({
+            id: msg.sender,
+            name: clinicName,
+            location: clinicLocation,
+            phoneNumber: clinicPhoneNumber,
+            clinicState: _clinicState.NotVerified
+        });
+    }
+    
+    
+    /* ====================== By Abhishek ========================== */
+    
+    uint public total_valid_volunteers;
+    uint public total_partially_vaccinated;
+    uint public total_fully_vaccinated;
+    uint public total_recovered;
+    uint public total_invalid_results;
+
+    
+    struct Dose {
+        uint dose_no;
+        uint vaccinationDate;
+        bool done;
+    }
+    
+    struct DoseResult {
+        bool published;
+        bool success;
+    }
+    
+    // Mapping of each dose given
+    mapping(uint => mapping(address => Dose)) doses;
+    
+    // Mapping for the dose result
+    mapping(uint => mapping(address => DoseResult)) doseResults;
+    
+    mapping(uint => mapping(address => string)) doseFeedbacks;
+    
+    
+    modifier onlyValidClinic {
+        require(clinics[msg.sender].clinicState == _clinicState.Verified, "Only an authorized clinic can do this!");
+        _;
+    }
+    
+    
+    function approveVolunteer(address volunteer) public onlyValidClinic {
+        
+        require (doesVolunteerExist(volunteer), "Volunteer doesn't exist");
+        
+        _approveVolunteer(volunteer);
+    }
+    
+    
+    
+    function markVaccinationDone(address volunteer, uint dose_no) public onlyValidClinic {
+        
+        require (isValidVolunteer(volunteer), "Not a valid volunteer");
+        require(dose_no <= NumberOfDose, "Invalid dose number");
+        
+        if (dose_no > 1) {
+            require(doses[dose_no-1][volunteer].done, "Last dose was not administered!");
+        }
+        
+        doses[dose_no][volunteer] = Dose({
+            dose_no: dose_no,
+            vaccinationDate: block.timestamp,
+            done: true
+        });
+        
+        if (dose_no == NumberOfDose) {
+            total_fully_vaccinated++;
+        } else if (dose_no < NumberOfDose) {
+            total_partially_vaccinated++;
+        }
+    }
+    
+    function markVaccinationResult(address volunteer, bool result) public onlyValidClinic {
+        
+        require (isValidVolunteer(volunteer), "Not a valid volunteer");
+        require(doses[NumberOfDose][volunteer].done, "Result can only be set after finel dose is administered!");
+        
+        doseResults[NumberOfDose][volunteer] = DoseResult({
+            published: true,
+            success: result
+        });
+        
+        if (result) {
+            total_recovered++;
+        }
+    }
+
+
+    function giveDoseFeedback(uint dose_no, string memory message) public {
+        require (isValidVolunteer(msg.sender), "Not a valid volunteer");
+        require (doseResults[dose_no][msg.sender].published, "Plz wait for dose result to be published!");
+        
+        doseFeedbacks[dose_no][msg.sender] = message;
+        total_invalid_results++;
+    }
+    
+    /* ============================================================= */
+    
+    
+}
+
